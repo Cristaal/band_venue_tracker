2ade9102bee69005f94e328b09af7ed092c904c0
Crystal Contreras-Grossman <crystalcontrerasgrossman@gmail.com>
2015-01-30 11:21:24 -0800

    add sinatra files

--- app.rb
+++ app.rb
@@ -2,3 +2,15 @@ require("bundler/setup")
 Bundler.require(:default)
 
 Dir[File.dirname(__FILE__) + '/lib/*.rb'].each { |file| require file }
+
+get('/') do
+  @venues = Venue.all()
+  erb(:index)
+end
+
+post("/venues") do
+  name = params.fetch("venue")
+  Venue.create({:name => name})
+  @venues = Venue.all()
+  erb(:index)
+end
