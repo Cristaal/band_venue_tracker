0aecc634cf729b7ab27d10bd902c9fed6bc44560
Crystal Contreras-Grossman <crystalcontrerasgrossman@gmail.com>
2015-01-30 15:51:10 -0800

    adds routes to add and list bands and venues

--- app.rb
+++ app.rb
@@ -4,13 +4,31 @@ Bundler.require(:default)
 Dir[File.dirname(__FILE__) + '/lib/*.rb'].each { |file| require file }
 
 get('/') do
+  @bands = Band.all()
   @venues = Venue.all()
   erb(:index)
 end
 
-post("/venues") do
+get('/bands') do
+  @bands = Band.all
+  erb(:bands)
+end
+
+post('/bands') do
+  name = params.fetch("band")
+  Band.create({:name => name})
+  @bands = Band.all()
+  erb(:bands)
+end
+
+get('/venues') do
+  @venues = Venue.all
+  erb(:venues)
+end
+
+post('/venues') do
   name = params.fetch("venue")
   Venue.create({:name => name})
   @venues = Venue.all()
-  erb(:index)
+  erb(:venues)
 end
