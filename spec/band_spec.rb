6a1a0e73de90401b5ffaea1b5f9fe088ce168552
Crystal Contreras-Grossman <crystalcontrerasgrossman@gmail.com>
2015-01-30 10:37:01 -0800

    Initial Commit

---
+++ spec/band_spec.rb
@@ -0,0 +1,15 @@
+require('spec_helper')
+
+describe(Band) do
+  it { should have_and_belong_to_many (:venues) }
+  it("validates the presence of name") do
+    band = Band.new({:name => ''})
+    expect(band.save()).to(eq(false))
+  end
+
+  it("will capitalize the name of the band") do
+    band = Band.create({:name => "the pixies"})
+    expect(band.name()).to(eq("The Pixies"))
+  end
+
+end
